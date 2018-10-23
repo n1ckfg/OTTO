ccc801c518a6df0f07ab7350bf49f97d8b162f8d
topisani <topisani@hamsterpoison.com>
2018-10-23 16:26:36 +0200

    Improved documentation

--- doxy-boot.js
+++ doxy-boot.js
@@ -78,7 +78,7 @@ $( document ).ready(function() {
 	$("table.fieldtable").addClass("table");
 	$(".fragment").addClass("well");
 
-  $(".memtitle").addClass("n-hidden");
+  //$(".memtitle").addClass("n-hidden");
   $(".memitem").addClass("panel panel-default");
 	$(".memproto").addClass("panel-heading");
 	$(".memdoc").addClass("panel-body");
@@ -98,5 +98,84 @@ $( document ).ready(function() {
 	$("div.ttname").addClass("panel-heading");
 	$("div.ttdef,div.ttdoc,div.ttdeci").addClass("panel-body");
 
+    (function(document, history, location) {
+      var HISTORY_SUPPORT = !!(history && history.pushState);
+
+      var anchorScrolls = {
+        ANCHOR_REGEX: /^#[^ ]+$/,
+        OFFSET_HEIGHT_PX: 60,
+
+        /**
+         * Establish events, and fix initial scroll position if a hash is provided.
+         */
+        init: function() {
+          this.scrollToCurrent();
+          $(window).on('hashchange', $.proxy(this, 'scrollToCurrent'));
+          $('body').on('click', 'a', $.proxy(this, 'delegateAnchors'));
+        },
+
+        /**
+         * Return the offset amount to deduct from the normal scroll position.
+         * Modify as appropriate to allow for dynamic calculations
+         */
+        getFixedOffset: function() {
+          return this.OFFSET_HEIGHT_PX;
+        },
+
+        /**
+         * If the provided href is an anchor which resolves to an element on the
+         * page, scroll to it.
+         * @param  {String} href
+         * @return {Boolean} - Was the href an anchor.
+         */
+        scrollIfAnchor: function(href, pushToHistory) {
+          var name, match, anchorOffset;
+
+          if(!this.ANCHOR_REGEX.test(href)) {
+            return false;
+          }
+
+          match = document.getElementById(href.slice(1));
+
+          if(match) {
+            anchorOffset = $(match).offset().top - this.getFixedOffset();
+            $('html, body').animate({ scrollTop: anchorOffset});
+
+            // Add the state to history as-per normal anchor links
+            if(HISTORY_SUPPORT && pushToHistory) {
+              history.pushState({}, document.title, location.pathname + href);
+            }
+          }
+
+          return !!match;
+        },
+        
+        /**
+         * Attempt to scroll to the current location's hash.
+         */
+        scrollToCurrent: function(e) { 
+          if(this.scrollIfAnchor(window.location.hash) && e) {
+          	e.preventDefault();
+          }
+        },
+
+        /**
+         * If the click event's target was an anchor, fix the scroll position.
+         */
+        delegateAnchors: function(e) {
+          var elem = e.target;
+
+          if(this.scrollIfAnchor(elem.getAttribute('href'), true)) {
+            e.preventDefault();
+          }
+        }
+      };
+
+    	$(document).ready($.proxy(anchorScrolls, 'init'));
+    })(window.document, window.history, window.location);
+
+
 
 });
+
+
